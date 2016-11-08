205ce3859fd30050787c10092e1819f270c708f4
Christopher Allan Webber <cwebber@dustycloud.org>
2016-11-08 10:41:53 -0600

    Add stub guix.scm

---
+++ guix.scm
@@ -0,0 +1,42 @@
+;;; activitypub.rocks website
+;;; Copyright © 2016 Christopher Allan Webber <cwebber@dustycloud.org>
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(use-modules (guix packages)
+             (guix licenses)
+             (guix git-download)
+             (guix build-system gnu)
+             (gnu packages)
+             (gnu packages autotools)
+             (gnu packages guile)
+             (gnu packages pkg-config)
+             (gnu packages texinfo))
+
+(define aprocks-site
+  (package
+    (name "aprocks-site")
+    (version "git")
+    (source #f)
+    (build-system gnu-build-system)
+    (synopsis #f)
+    (description #f)
+    (license gpl3+)
+    (home-page "http://activitypub.rocks")
+    (inputs
+     `(("guile" ,guile-2.0)
+       ("haunt" ,haunt)
+       ("guile-reader" ,guile-reader)))))
+
+aprocks-site
